1b45081b3aa22dad3cf607e5f5e385b21e0dd48f
Emil <emil_georgiev_it@abv.bg>
2018-12-29 14:31:44 +0000

    Add additional components

--- src/app/components/cars/cars.module.ts
+++ src/app/components/cars/cars.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarsComponent } from '../cars/cars/cars.component';
 import { CarsRoutingModule } from './cars.routing';
+import { CarsDetailsComponent } from './cars-details/cars-details.component';
+import { CarsCreateComponent } from './cars-create/cars-create.component';
+import { CarsDeleteComponent } from './cars-delete/cars-delete.component';
+import { CarsEditComponent } from './cars-edit/cars-edit.component';
 
 
 @NgModule({
@@ -10,7 +14,11 @@ import { CarsRoutingModule } from './cars.routing';
     CarsRoutingModule
   ],
   declarations: [
-    CarsComponent
+    CarsComponent,
+    CarsDetailsComponent,
+    CarsCreateComponent,
+    CarsDeleteComponent,
+    CarsEditComponent
   ],
   exports: [
     CarsComponent
--- src/app/components/cars/cars.routing.ts
+++ src/app/components/cars/cars.routing.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { CarsComponent } from './cars/cars.component';
+import { CarsEditComponent } from './cars-edit/cars-edit.component';
+import { CarsDeleteComponent } from './cars-delete/cars-delete.component';
+import { CarsCreateComponent } from './cars-create/cars-create.component';
+import { CarsDetailsComponent } from './cars-details/cars-details.component';
 
 const carsRoutes: Routes = [
   { path: '', component: CarsComponent },
+  { path: 'create', component: CarsCreateComponent },
+  { path: 'delete', component: CarsDeleteComponent },
+  { path: 'details/:id', component: CarsDetailsComponent },
+  { path: 'edit', component: CarsEditComponent },
+
 ];
 
 @NgModule({
@@ -13,7 +22,11 @@ const carsRoutes: Routes = [
     RouterModule.forChild(carsRoutes)
   ],
   declarations: [
-    CarsComponent
+    CarsComponent,
+    CarsCreateComponent,
+    CarsDeleteComponent,
+    CarsEditComponent,
+    CarsDetailsComponent
   ],
   exports: [
     RouterModule
